260b83ba06e15ffed1623d9aa0b1b2e8b4a24381
Toby Hogan <tobyhogan44@gmail.com>
2021-10-19 20:47:27 +0100

    Completed project

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 spacer = "=" * 63
 
-
-def get_range():
+def get_number():
     while True:
         print(spacer)
-        print("What is the range of happy numbers from 0 that you want to find? ")
+        print("What number do you want to check?")
         print(spacer)
-        range = input(">>> ")
+        num = input(">>> ")
 
         try:
-            range = int(range)
-            return range
+            num = int(num)
+            return num
 
         except:
+            print(spacer)
             print("Invalid input, please try again.")
             print(spacer)
             print("Do you want to exit to the main menu?")
@@ -21,10 +21,58 @@ def get_range():
 
 
 def is_happy(number):
-    iterations = 0
+    try:
+        num_as_string = str(number)
+        num_digits = len(num_as_string)
 
-    while (number != 0) and (iterations < 1000):
-        pass
+        total_value = 0
+
+        if number == 1:
+            return 1
+
+        else:
+            for i in range(num_digits):
+                total_value += int(num_as_string[i]) ** 2
+
+            return is_happy(total_value)
+        
+
+    except:
+        return 0
+            
+def remain_question():
+    while True:
+        print(spacer)
+        print("Do you want to check another number of quit?")
+        print("")
+        print("1 = Check another number.")
+        print("2 = Quit.")
+        print(spacer)
+        check_again = input(">>> ")
+
+        if check_again == "1":
+            return True
+        
+        elif check_again == "2":
+            return False
+
+        else:
+            print(spacer)
+            print("Invalid input, please try again.")
 
 
-is_happy(20)
+while True:
+    num = get_number()
+    print(spacer)
+    
+    if is_happy(num):
+        print(f"{num} is a happy number :)")
+
+    else:
+        print(f"{num} is not a happy number :(")
+
+    if remain_question():
+        continue
+    
+    else:
+        break

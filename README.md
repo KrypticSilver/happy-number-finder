92dd99f9737fcba781b61a5b7bcdcb105d73fef6
Toby Hogan <tobyhogan44@gmail.com>
2021-10-19 17:13:56 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Happy Number Finder
-
+<h1>Happy Number Finder</h1>
 <h2>The aim of this project is to make a program that can find all the happy numbers between a specified range.</h2>
+<h2>A happy number is a number that eventually reaches 1 when the sum of the squares of each comprising digit is taken and added together. This process repeats until the number eventually reaches one or it doesn't.
--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("happy number")
+spacer = "=" * 60
+
+
+def get_range():
+    while True:
+        print(spacer)
+        print("What is the range of happy numbers from 0 you want to find? ")
+        print(spacer)
+        range = input(">>> ")
+        try:
+            range = int(range)
+            return range
+        
+        except:
+            print("Invalid input, please try again.")
+
+def is_happy(number):
+    iterations = 0
+
+    while (number != 0) and (iterations < 1000):
+        
+
+
+

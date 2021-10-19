81149d969ea2e6a26bd8f4928ff9f8d2a5b3168c
Toby Hogan <tobyhogan44@gmail.com>
2021-10-19 18:19:08 +0100

    Improved structure

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
-spacer = "=" * 60
+spacer = "=" * 63
 
 
 def get_range():
     while True:
         print(spacer)
-        print("What is the range of happy numbers from 0 you want to find? ")
+        print("What is the range of happy numbers from 0 that you want to find? ")
         print(spacer)
         range = input(">>> ")
+
         try:
             range = int(range)
             return range
 
         except:
             print("Invalid input, please try again.")
+            print(spacer)
+            print("Do you want to exit to the main menu?")
+            print("1 = Exit to main menu.")
+            print("2 = Try again.")
 
 
 def is_happy(number):
     iterations = 0
 
     while (number != 0) and (iterations < 1000):
+        pass
+
+
+is_happy(20)
